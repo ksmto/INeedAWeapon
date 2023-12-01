21b627946ae42efc91a492d9c4c78b90e397305b
Kishimoto <wyatt.woodley@outlook.com>
2023-11-30 21:18:09 -0500

    Scopes: color chaning reticles

---
+++ ItemModuleScope.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ThunderRoad;
+using UnityEngine;
+
+namespace INeedAWeapon {
+    internal class ItemModuleScope : ItemModule {
+        public override void OnItemLoaded(Item item) {
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<ScopeBehaviour>();
+        }
+    }
+}
---
+++ ScopeBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ThunderRoad;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace INeedAWeapon {
+    public class ScopeBehaviour : MonoBehaviour {
+        private Item item;
+        private Image blueReticle;
+        private Color originalColor;
+
+        private void Start() {
+            InitializeComponents();
+        }
+
+        private void Update() {
+            UpdateReticleColor();
+        }
+
+        private void InitializeComponents() {
+            item = GetComponent<Item>();
+
+            if (item != null) {
+                Transform scopeReticleTransform = FindScopeReticleTransform();
+
+                if (scopeReticleTransform != null) {
+                    blueReticle = scopeReticleTransform.GetComponent<Image>();
+
+                    if (blueReticle != null) {
+                        originalColor = blueReticle.color;
+                    } else {
+                        Debug.LogError("ScopeReticle does not have Image component.");
+                    }
+                } else {
+                    Debug.LogError("ScopeReticle not found in children.");
+                }
+            }
+        }
+
+        private Transform FindScopeReticleTransform() {
+            Transform scopeTransform = item.transform?.Find("Battle Rifle")?.Find("Scope")?.Find("ScopeCamera")?.Find("Canvas")?.Find("ScopeBackgroud")?.Find("ScopeImage")?.Find("ScopeReticle");
+            return scopeTransform;
+        }
+
+        private void UpdateReticleColor() {
+            if (item != null) {
+                if (Physics.Raycast(item.transform.position, item.transform.forward, out RaycastHit hit)) {
+                    HandleHitCreature(hit);
+                } else {
+                    ResetReticleColor();
+                }
+            }
+        }
+
+        private void HandleHitCreature(RaycastHit hit) {
+            Creature creature = hit.collider.GetComponentInParent<Creature>();
+            if (creature != null) {
+                Debug.Log("Hit creature");
+                blueReticle.material.color = Color.red;
+                Debug.Log("Changed color to red");
+            }
+        }
+
+        private void ResetReticleColor() {
+            if (blueReticle != null && blueReticle.material != null) {
+                blueReticle.material.color = originalColor;
+                Debug.Log("Changed color to original");
+            }
+        }
+    }
+}
