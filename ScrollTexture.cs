cc1c11ca2284ce9e51bc417e5e7328cc52d508e0
Kishimoto <wyatt.woodley@outlook.com>
2023-12-02 00:05:31 -0500

    namespace

--- ScrollTexture.cs
+++ ScrollTexture.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 
-public class ScrollTexture : MonoBehaviour
-{
-    public float ScrollX = 0.5f;
-    public float ScrollY = 0.5f;
-    public float RimScrollX = 0.5f;
-    public float RimScrollY = 0.5f;
+namespace INeedAWeapon {
+    public class ScrollTexture : MonoBehaviour {
+        public float ScrollX = 0.5f;
+        public float ScrollY = 0.5f;
+        public float RimScrollX = 0.5f;
+        public float RimScrollY = 0.5f;
 
-    private void Update()
-    {
-        float OffsetX = Time.time * ScrollX;
-        float OffsetY = Time.time * ScrollY;
-        float RimOffsetX = Time.time * RimScrollX;
-        float RimOffsetY = Time.time * RimScrollY;
+        private void Update() {
+            float OffsetX = Time.time * ScrollX;
+            float OffsetY = Time.time * ScrollY;
+            float RimOffsetX = Time.time * RimScrollX;
+            float RimOffsetY = Time.time * RimScrollY;
 
-        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
-        GetComponent<Renderer>().material.SetTextureOffset("_rimtexture", new Vector2(RimOffsetX, RimOffsetY));
+            GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+            GetComponent<Renderer>().material.SetTextureOffset("_rimtexture", new Vector2(RimOffsetX, RimOffsetY));
+        }
     }
 }
