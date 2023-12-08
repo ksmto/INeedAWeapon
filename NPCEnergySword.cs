d92bc316d778a4b69f9cf65a5768cad67454b4e5
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2023-12-08 08:48:44 -0800

    Add files via upload

---
+++ NPCEnergySword.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using ThunderRoad.AI.Action;
+using UnityEngine;
+
+namespace INeedAWeapon
+{
+    public class NPCEnergySwordModule : ItemModule
+    {
+        public override void OnItemLoaded(Item item)
+        {
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<NPCEnergySword>();
+        }
+
+
+        public class NPCEnergySword : ThunderBehaviour
+        {
+            private Item item;
+            AnimatorParamController anim;
+            public void Start()
+            {
+                EventManager.onItemEquip += NpcHandCheck;
+                if(item != null )
+                {
+                    anim = item.GetComponent<AnimatorParamController>();
+                }
+            }
+
+            private void NpcHandCheck(Item item)
+            {
+                if (item.mainHandler != null)
+                {
+                    if (!item.mainHandler.creature.isPlayer)
+                    {
+                        anim.SetTrigger("npcSword");
+                    }
+                }
+            }
+        }
+    }
+}
