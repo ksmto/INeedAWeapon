fc89c114f0f52ffe8bb5a68102fd4e5c1120c593
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2023-12-17 22:50:04 -0600

    Update Cortana.cs

--- Cortana.cs
+++ Cortana.cs
@@ -1,5 +1,13 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using ThunderRoad;
+using UnityEngine;
+using Random = System.Random;
+
 
 namespace INeedAWeapon
 {
@@ -14,26 +22,27 @@ namespace INeedAWeapon
 
     public class CortanaChip : ThunderBehaviour
     {
-        private Item chip;
-        private CortanaAnnouncer announcer;
 
+
+        private Item item;
+        CortanaAnnouncer announcer;
         protected override void ManagedOnEnable()
         {
             base.ManagedOnEnable();
-            chip = GetComponent<Item>();
-            if (chip != null)
+            item = GetComponent<Item>();
+
+            if (item != null)
             {
                 CortanaAnnouncer announcer = new CortanaAnnouncer();
-                EventManager.onEdibleConsumed += CortanaChipEaten;
-                announcer.boolCortanaActive = false;
-            }
+                EventManager.onEdibleConsumed += CortanaChipEaten; }
+
         }
+
 
         private void CortanaChipEaten(Item edible, Creature consumer, EventTime eventTime)
         {
             if (consumer != null && consumer.isPlayer)
             {
-                announcer.boolCortanaActive = true;
                 announcer.CortanaGreeting();
             }
         }
@@ -41,58 +50,67 @@ namespace INeedAWeapon
 
     public class CortanaAnnouncer : HaloEvents
     {
-        public bool boolCortanaActive;
-        private int intQuoteRoll;
-        private Random random = new Random();
-        private EffectData Cortana_DeathEffect, Cortana_LowHPEffect, Cortana_HurtEffect, Cortana_EnemyDamagedEffect, Cortana_EnemyWarnEffect, Cortana_KillEffect, Cortana_KillHeadshotEffect, Cortana_SpawnGreetEffect;
-        private EffectInstance Cortana_Death, Cortana_LowHP, Cortana_Hurt, Cortana_EnemyDamaged, Cortana_EnemyWarn, Cortana_Kill, Cortana_KillHeadshot, Cortana_SpawnGreet;
-
+        int intQuoteRoll;
+        Random random = new Random();
+        bool isPlaying;
+        private EffectData Cortana_DeathEffect, Cortana_LowHPEffect, Cortana_EnemyWarnEffect, Cortana_KillEffect, Cortana_KillHeadshotEffect, Cortana_SpawnGreetEffect;
+        private EffectInstance CortanaPlayerDeath, Cortana_LowHP, CortanaPlayerDamaged, CortanaEnemyWarn, Cortana_Kill, Cortana_Headshot, Cortana_SpawnGreet;
         public override void OnCatalogRefresh(EventTime eventTime)
         {
             Cortana_DeathEffect = Catalog.GetData<EffectData>("INAWCortanaDeath");
             Cortana_LowHPEffect = Catalog.GetData<EffectData>("INAWCortanaLowHp");
-            Cortana_HurtEffect = Catalog.GetData<EffectData>("INAWCortanaHurt");
-            Cortana_EnemyDamagedEffect = Catalog.GetData<EffectData>("INAWCortanaEnemyDamage");
             Cortana_EnemyWarnEffect = Catalog.GetData<EffectData>("INAWCortanaEnemyWarn");
             Cortana_KillEffect = Catalog.GetData<EffectData>("INAWCortanaKill");
             Cortana_KillHeadshotEffect = Catalog.GetData<EffectData>("INAWCortanaKillHeadshot");
             Cortana_SpawnGreetEffect = Catalog.GetData<EffectData>("INAWCortanaGreet");
         }
 
+
         /* EVENTS */
         public override void OnCreatureHit(Creature creature, CollisionInstance collisionInstance, EventTime eventTime)
         {
-            if (boolCortanaActive)
+            if (ModOptions.cortanaActive)
             {
-                if (creature.isPlayer)
+
+                if (CortanaRandom())
                 {
-                    if (CortanaRandom())
+                    
+                    if (creature.isPlayer)
                     {
+                        Debug.Log("Cortana Player hit triggered");
                         CortanaPlayerHurt();
                     }
                 }
+
             }
         }
-
         public override void OnCreatureKill(Creature creature, Player player, CollisionInstance collisionInstance, EventTime eventTime)
         {
-            if (boolCortanaActive)
             {
-                if (CortanaRandom())
+                if (ModOptions.cortanaActive)
                 {
-                    CortanaKills(collisionInstance, creature);
+                    {
+                        if (CortanaRandom())
+                        {
+                            Debug.Log("cortana creature kill triggered");
+                            CortanaKills(collisionInstance, creature);
+                        }
+                    }
+
                 }
+
             }
         }
-
         public override void OnCreatureSpawn(Creature creature)
         {
-            if (boolCortanaActive)
+            if (ModOptions.cortanaActive && !creature.isPlayer)
             {
                 if (CortanaRandom())
                 {
+
                     CortanaEnemyWarning();
                 }
+
             }
         }
 
@@ -100,63 +118,117 @@ namespace INeedAWeapon
         public bool CortanaRandom()
         {
             intQuoteRoll = random.Next(100 + 1);
-            return UnityEngine.Random.Range(0, 10) >= intQuoteRoll;
-            // return ModOptions.quoteChance >= intQuoteRoll;
-        }
-
-        public void CortanaEnemyDamage()
-        {
-            EffectInstance Cortana_EnemyDamage = Cortana_EnemyDamagedEffect?.Spawn(Player.local.creature.transform);
-            Cortana_EnemyDamage.Play();
+            Debug.Log("Cortana Random roll = " + intQuoteRoll);
+            if (ModOptions.quoteChance <= intQuoteRoll)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public void CortanaGreeting()
         {
-            EffectInstance Cortana_SpawnGreetInstance = Cortana_SpawnGreetEffect?.Spawn(Player.local.creature.transform);
-            Cortana_SpawnGreetInstance.Play();
+            if (!isPlaying)
+            {
+                GameManager.local.StartCoroutine(CortanaGreetRoutine());
+            }
         }
-
         public void CortanaPlayerHurt()
         {
-            if (boolCortanaActive)
+            if (!isPlaying)
             {
                 if (Player.currentCreature.currentHealth / Player.currentCreature.maxHealth * 100 <= 25f)
                 {
-                    EffectInstance CortanaPlayerHPWarn = Cortana_LowHPEffect?.Spawn(Player.local.creature.transform);
-                    CortanaPlayerHPWarn.Play();
+                    GameManager.local.StartCoroutine(PlayerCriticalHurtRoutine());
+
+                }
+            }
+
+        }
+        public void CortanaKills(CollisionInstance collisionInstance, Creature creature)
+        {
+            if (!isPlaying)
+            {
+                if (creature.isPlayer)
+                {
+                    GameManager.local.StartCoroutine(PlayerKilledRoutine());
+                }
+
+                else if (collisionInstance.damageStruct.hitRagdollPart.ragdoll.headPart)
+                {
+                    GameManager.local.StartCoroutine(CreatureHeadShotRoutine());
                 }
                 else
                 {
-                    EffectInstance CortanaPlayerHurt = Cortana_HurtEffect?.Spawn(Player.local.creature.transform);
-                    CortanaPlayerHurt.Play();
+                    GameManager.local.StartCoroutine(CreatureKillRoutine());
                 }
             }
-        }
 
-        public void CortanaKills(CollisionInstance collisionInstance, Creature creature)
-        {
-            if (creature.isPlayer)
-            {
-                EffectInstance CortanaPlayerDeath = Cortana_DeathEffect?.Spawn(Player.local.creature.transform);
-                CortanaPlayerDeath.Play();
-            }
-
-            else if (collisionInstance.damageStruct.hitRagdollPart.ragdoll.headPart)
-            {
-                EffectInstance Cortana_Headshot = Cortana_KillHeadshotEffect?.Spawn(Player.local.creature.transform);
-                Cortana_Headshot.Play();
-            }
-            else
-            {
-                EffectInstance Cortana_Kill = Cortana_KillHeadshotEffect?.Spawn(Player.local.creature.transform);
-                Cortana_Kill.Play();
-            }
         }
 
         public void CortanaEnemyWarning()
         {
-            EffectInstance CortanaEnemyWarn = Cortana_EnemyWarnEffect?.Spawn(Player.local.creature.transform);
+            if (!isPlaying)
+            {
+                GameManager.local.StartCoroutine(EnemySpawnWarningRoutine());
+            }
+
+        }
+
+        public IEnumerator PlayerCriticalHurtRoutine()
+        {
+            isPlaying = true;
+            Cortana_LowHP = Cortana_LowHPEffect?.Spawn(Player.local.creature.transform);
+            Cortana_LowHP. Play();
+            yield return new WaitForSeconds(ModOptions.quoteDelay);
+            isPlaying = false;
+            yield return null;
+        }
+
+        public IEnumerator CreatureKillRoutine()
+        {
+            isPlaying = true;
+            Cortana_Kill = Cortana_KillEffect?.Spawn(Player.local.creature.transform);
+            Cortana_Kill.Play();
+            yield return new WaitForSeconds(ModOptions.quoteDelay);
+            isPlaying = false;
+            yield return null;
+        }
+        public IEnumerator CreatureHeadShotRoutine()
+        {
+            isPlaying = true;
+            Cortana_Headshot = Cortana_KillHeadshotEffect?.Spawn(Player.local.creature.transform);
+            Cortana_Headshot.Play();
+            yield return new WaitForSeconds(ModOptions.quoteDelay);
+            isPlaying = false;
+            yield return null;
+        }
+        public IEnumerator PlayerKilledRoutine()
+        {
+            isPlaying = true;
+            CortanaPlayerDeath = Cortana_DeathEffect?.Spawn(Player.local.creature.transform);
+            CortanaPlayerDeath.Play();
+            yield return new WaitForSeconds(ModOptions.quoteDelay);
+            isPlaying = false;
+            yield return null;
+        }
+        public IEnumerator CortanaGreetRoutine()
+        {
+            isPlaying = true;
+            Cortana_SpawnGreet = Cortana_SpawnGreetEffect?.Spawn(Player.local.creature.transform);
+            Cortana_SpawnGreet.Play();
+            yield return new WaitForSeconds(ModOptions.quoteDelay);
+            isPlaying = false;
+            yield return null;
+        }
+        public IEnumerator EnemySpawnWarningRoutine()
+        {
+            isPlaying = true;
+            CortanaEnemyWarn = Cortana_EnemyWarnEffect?.Spawn(Player.local.creature.transform);
             CortanaEnemyWarn.Play();
+            yield return new WaitForSeconds(ModOptions.quoteDelay);
+            isPlaying = false;
+            yield return null;
         }
     }
 }
