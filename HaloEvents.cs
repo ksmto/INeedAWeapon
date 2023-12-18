9835d28dc4d4c1beb24f0a791b750e19a0f0c2ac
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2023-12-17 22:50:59 -0600

    Update HaloEvents.cs

--- HaloEvents.cs
+++ HaloEvents.cs
@@ -1,19 +1,21 @@
-﻿using ThunderRoad;
+using ThunderRoad;
 
-namespace INeedAWeapon
-{
-    public class HaloEvents : ThunderScript
-    {
-        public override void ScriptEnable()
-        {
-            EventManager.onCreatureSpawn += OnCreatureSpawn;
-            EventManager.onCreatureHit += OnCreatureHit;
+namespace INeedAWeapon {
+    public class HaloEvents : ThunderScript {
+
+        public override void ScriptLoaded(ModManager.ModData modData) {
+            base.ScriptLoaded(modData);
+        }
+
+        public override void ScriptEnable() {
             EventManager.onCreatureKill += OnCreatureKill;
             EventManager.onLevelLoad += OnMapChange;
             EventManager.onCatalogRefresh += OnCatalogRefresh;
+            EventManager.onCreatureHit += OnCreatureHit;
+            EventManager.onCreatureSpawn += OnCreatureSpawn;
         }
 
-        public virtual void OnCreatureSpawn(Creature creature) { }
+        public virtual void OnCreatureSpawn(Creature creature){}
         public virtual void OnCreatureHit(Creature creature, CollisionInstance collisionInstance, EventTime eventTime) { }
         public virtual void OnCreatureKill(Creature creature, Player player, CollisionInstance collisionInstance, EventTime eventTime) { }
         public virtual void OnMapChange(LevelData levelData, EventTime eventTime) { }
