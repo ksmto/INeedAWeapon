8eb04a17abbbabc1a1ac983854e415057c2b7831
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2024-01-19 15:45:16 -0600

    Update HaloEvents.cs
    Added on creature heal and AIRandom

--- HaloEvents.cs
+++ HaloEvents.cs
@@ -1,9 +1,13 @@
+using System;
 using ThunderRoad;
+using UnityEngine;using Random = System.Random;
 
 namespace INeedAWeapon
 {
     public class HaloEvents : ThunderScript
     {
+        int intQuoteRoll;
+        Random random = new Random();
         public override void ScriptEnable()
         {
             EventManager.onCreatureKill += OnCreatureKill;
@@ -11,12 +15,25 @@ namespace INeedAWeapon
             EventManager.onCatalogRefresh += OnCatalogRefresh;
             EventManager.onCreatureHit += OnCreatureHit;
             EventManager.onCreatureSpawn += OnCreatureSpawn;
+            EventManager.onCreatureHeal += OnCreatureHeal;
         }
 
+        public virtual void OnCreatureHeal(Creature creature, float heal, Creature healer, EventTime eventTime){}
         public virtual void OnCreatureSpawn(Creature creature) { }
         public virtual void OnCreatureHit(Creature creature, CollisionInstance collisionInstance, EventTime eventTime) { }
         public virtual void OnCreatureKill(Creature creature, Player player, CollisionInstance collisionInstance, EventTime eventTime) { }
         public virtual void OnMapChange(LevelData levelData, EventTime eventTime) { }
         public virtual void OnCatalogRefresh(EventTime eventTime) { }
+
+
+        public bool AiRandom()
+        {
+            intQuoteRoll = random.Next(100 + 1);
+            if (ModOptions.quoteChance <= intQuoteRoll)
+            {
+                return true;
+            }
+            else return false;
+        }
     }
 }
